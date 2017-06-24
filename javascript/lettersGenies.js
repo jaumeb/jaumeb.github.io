7cc1cc70acb27cb001c7fd3e6c66980720b580b5
Jaume <jbserveis@icloud.com>
2017-06-25 00:04:10 +0200

    canvis per fer-ho més ES6

--- javascript/lettersGenies.js
+++ javascript/lettersGenies.js
@@ -3,38 +3,70 @@
 // modern browsers (including IE9+), and is slightly more robust than `onload`.
 // More here: http://stackoverflow.com/a/21814964/1028230
 document.addEventListener('DOMContentLoaded', function () {
-    var inputNumbers = document.getElementById('inputNumbers') // input number
-    var buttonEscriu = document.getElementById('buttonEscriu') // input button
-    var divLletresGeni = document.getElementById('lletresGeni') // div
-    var divLletresGenis72 = document.getElementById('lletresGenis72') // div    
+    const inputNumbers = document.getElementById('inputNumbers') // input number
+    const buttonEscriu = document.getElementById('buttonEscriu') // input button
+    // div to place the name of the genius one by one each time
+    const divLletresGeni = document.getElementById('lletresGeni')
+    // div to place the table of the 72 genii names
+    const divLletresGenis72 = document.getElementById('lletresGenis72')
+    // array with the 72 Genii names of 5 letters each
+    // build whith the code in https://gist.github.com/jaumeb/04d74de20d7ea68fc5dc
+    const genis72 = [
+        'והויה', 'יליאל', 'סיטאל', 'עלמיה', 'מהשיה', 'ללהאל', 'אכאיה', 'כהתאל',
+        'הזיאל', 'אלדיה', 'לאויה', 'ההעיה', 'יזלאל', 'מבהאל', 'הריאל', 'הקמיה',
+        'לאויה', 'כליאל', 'לוויה', 'פהליה', 'נלכאל', 'יייאל', 'מלהאל', 'חהויה',
+        'נתהיה', 'האאיה', 'ירתאל', 'שאהיה', 'רייאל', 'אומאל', 'לכבאל', 'ושריה',
+        'יחויה', 'להחיה', 'כוקיה', 'מנדאל', 'אניאל', 'חעמיה', 'רהעאל', 'ייזאל',
+        'הההאל', 'מיכאל', 'ווליה', 'ילהיה', 'סאליה', 'עריאל', 'עשליה', 'מיהאל',
+        'והואל', 'דניאל', 'החשיה', 'עממיה', 'ננאאל', 'ניתאל', 'מבהיה', 'פויאל',
+        'נממיה', 'יילאל', 'הרחאל', 'מצראל', 'ומבאל', 'יההאל', 'ענואל', 'מחיאל',
+        'דמביה', 'מנקאל', 'איעאל', 'חבויה', 'ראהאל', 'יבמיה', 'הייאל', 'מומיה'
+    ]
 
-    if (inputNumbers && buttonEscriu) {
-        // capture Return key
-        inputNumbers.addEventListener('keydown', function(e) {
-            var key = e.keyCode || e.which // valid in all browsers
-            if (key === 13) {
-                buttonEscriu.click()
-                e.preventDefault() // for Firefox in a form
-            }
-        })
-        buttonEscriu.addEventListener('click', function () {
-	    var numG = inputNumbers.value
-            if (isValid(numG)) {writeLetters(numG)}
-        })
-    }
+    // puts cursor in the field for input the number
+    inputNumbers.focus()
+    // capture Return key
+    inputNumbers.addEventListener('keydown', function (e) {
+        const key = e.keyCode || e.which // valid in all browsers
+        if (key === 13) {
+            buttonEscriu.click()
+            e.preventDefault() // for Firefox in a form
+        }
+    })
 
-
+    /**
+     * checks out if input is a valid number
+     * @param {integer} numG el número del geni a mostrar
+     *  des del 0, table amb tots el Genis, al 72, cada un dels Genis individualment
+     */
     function isValid(numG) {
-        var numericExpression = /^[0-9]{1,2}$/
-        if (numG.match(numericExpression)) {
-	    return true
+        // only numbers of 1 or 2 digits
+        const numericExpression = /^[0-9]{1,2}$/
+        // from 0 to 72
+        if (numG.match(numericExpression) && numG < 73) {
+            return true
         } else {
-            alert("només números de l'1 al 72\n\n" + 
-            "escriu 0 per a la taula completa")
+            alert("només números de l'1 al 72\n\n" +
+                "escriu 0 per a la taula completa")
             inputNumbers.focus()
         }
     }
-    
+
+    /**
+     * if there is a valid numbers in the input, execute the function that write the letters.
+     */
+    buttonEscriu.addEventListener('click', function () {
+        let numG = inputNumbers.value
+        if (isValid(numG)) {
+            writeLetters(numG)
+        }
+    })
+
+    /**
+     * write the letters of the Genii,
+     * individually or the whole 72 in a table
+     * @param {number} numG
+     */
     function writeLetters(numG) {
         if (numG == 0) {
             lletres72G(numG)
@@ -43,89 +75,61 @@ document.addEventListener('DOMContentLoaded', function () {
             lletres1G(numG)
         }
     }
-    
+    /**
+     * write the 5 letters of the Genie
+     * @param {integer} numG
+     */
     function lletres1G(numG) {
+        // deletes leading zeros in numG
+        numG = numG.replace(/^[0]+/g, "")
         divLletresGeni.innerHTML += '<h1>Geni ' +
-        numG + ': ' +
-        genis[parseInt(numG - 1, 10)] +
-        '</h1>'
+            numG + ': ' +
+            genis72[numG - 1] +
+            '</h1>'
     }
-    
-    function lletres72G(numG) {
-        var table72G = '\
-            <table> \
+
+    /**
+     * builds the table of 72 Genii
+     */
+    function lletres72G() {
+        let table72G =
+            '<table> \
                 <caption>Els 72 Genis</caption> \
                 <thead> \
-                <tr> \
-                <th>#</td> \
-                <th>lletres</td> \
-                <th>#</td> \
-                <th>lletres</td> \
-                </tr> \
+                    <tr> \
+                        <th>#</td> <th>Serafins</td> \
+                        <th>#</td> <th>Querubins</td> \
+                        <th>#</td> <th>Trons</td> \
+                        <th>#</td> <th>Dominacions</td> \
+                        <th>#</td> <th>Virtuts</td> \
+                        <th>#</td> <th>Potestats</td> \
+                        <th>#</td> <th>Principats</td> \
+                        <th>#</td> <th>Arcàngels</td> \
+                        <th>#</td> <th>Àngels</td> \
+                    </tr> \
                 </thead> \
-                <tbody> '
-        for (var i = 1; i < 37; i++) {
-            // builds the two column table
-            table72G += '\
-                <tr> \
-                <td>' + i + ' </td> \
-                <td>' + genis[i - 1] + '</td> \
-                <td>' + (i + 36) + ' </td> \
-                <td>' + genis[i + 35] + '</td></tr> '
+                <tbody>' // an order of angels in each column
+        for (let i = 1; i < 9; i++) {
+            // builds an eight column table
+            table72G +=
+                    '<tr>\
+                        <td>' + i + '       </td> <td>' + genis72[i - 1] + '</td> \
+                        <td>' + (i + 8) + ' </td> <td>' + genis72[i + 7] + '</td> \
+                        <td>' + (i + 16) + ' </td> <td>' + genis72[i + 15] + '</td> \
+                        <td>' + (i + 24) + ' </td> <td>' + genis72[i + 23] + '</td> \
+                        <td>' + (i + 32) + ' </td> <td>' + genis72[i + 31] + '</td> \
+                        <td>' + (i + 40) + ' </td> <td>' + genis72[i + 39] + '</td> \
+                        <td>' + (i + 48) + ' </td> <td>' + genis72[i + 47] + '</td> \
+                        <td>' + (i + 56) + ' </td> <td>' + genis72[i + 55] + '</td> \
+                        <td>' + (i + 64) + ' </td> <td>' + genis72[i + 63] + '</td> \
+                    </tr>'
         }
-        table72G += '\
-                </tbody>\
+
+        table72G +=
+                '</tbody> \
             </table>'
         divLletresGenis72.innerHTML = table72G
     }
-    
-    
-    // https://en.wikipedia.org/wiki/Shemhamphorasch
-    // builds de names of the 72 genii of Kabbalah
-    // the three constants below are each of the 72 letters
-    // of Exodus, chapter 14, verses 19, 20, 21
-    var EXODE1419 = 'ויסעמלאךהאלהיםההלךלפנימחנהישראלוילךמאחריהםויסעעמודהענןמפניהםויעמדמאחריהם'
-    var EXODE1420 = 'ויבאביןמחנהמצריםוביןמחנהישראלויהיהענןוהחשךויאראתהלילהולאקרבזהאלזהכלהלילה'
-    var EXODE1421 = 'ויטמשהאתידועלהיםויולךיהוהאתהיםברוחקדיםעזהכלהלילהוישםאתהיםלחרבהויבקעוהמים'
-    // F1 contains the number of the genie whose name is finished in יה
-    var F1 = [1, 4, 5, 7, 10, 11, 12, 16, 17, 19, 20, 24, 25, 26, 28, 32, 33,
-    34, 35, 38, 43, 44, 45, 47, 51, 52, 55, 57, 65, 68, 70, 72]
-    // array that must contain the letters of the genii
-    var genis = []
-    // finalsNo() receives a letter that if it's a 'final',
-    // it's changed to the 'regular'
-    function finalsNo (fno) {
-        // busca el valor de la lletra final
-        var lletresFinals = { 'ך': 'כ' ,'ם': 'מ' ,'ן': 'נ' ,'ף': 'פ' , 'ץ': 'צ' }[fno]
-        // si fno és una lletra final
-        if (lletresFinals) {
-            return lletresFinals
-        } else {
-            return fno
-        }
-    }
-    
-    // grab each letter of the verse of exodus
-    // which will build the name of the genie,
-    // add the suffix אל / יה that corresponds to.
-    ;
-    (function geniNom () {
-        var finals // to save the final letters
-        var f1ih = 0 // var for the following 'for'
-        for (var i = 0; i < 72; i++) {
-            var lletra1 = finalsNo(EXODE1419[i])
-            var lletra2 = finalsNo(EXODE1420[EXODE1420.length - (i + 1)])
-            var lletra3 = finalsNo(EXODE1421[i])
-            // If i + 1 is equal to the number of the genius in f1
-            if (i + 1 === F1[f1ih]) {
-                finals = 'יה'
-                f1ih++ // increase the value to point to the next index of F1
-            } else {
-                finals = 'אל'
-            }
-            // saves the letters of the genius in the array genis
-            genis[i] = lletra1 + lletra2 + lletra3 + finals
-        }
-    })(); // fills the array genis
+
 
 }) // DOMContentLoaded

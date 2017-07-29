b9b45c2f7bd7209cf27d4b3ddf9e87c9e25d0b6d
Jaume <jbserveis@icloud.com>
2017-07-29 14:40:11 +0200

    lettersGenies.js compatible ES5 via babel

--- javascript/lettersGenies.js
+++ javascript/lettersGenies.js
@@ -1,50 +1,41 @@
-'use strict'
+'use strict';
 // The document.addEventListener replicates $(document).ready() for
 // modern browsers (including IE9+), and is slightly more robust than `onload`.
 // More here: http://stackoverflow.com/a/21814964/1028230
+
 document.addEventListener('DOMContentLoaded', function () {
-    const inputNumbers = document.getElementById('inputNumbers') // input number
-    const buttonEscriu = document.getElementById('buttonEscriu') // input button
+    var inputNumbers = document.getElementById('inputNumbers'); // input number
+    var buttonEscriu = document.getElementById('buttonEscriu'); // input button
     // div to place the name of the genius one by one each time
-    const divLletresGeni = document.getElementById('lletresGeni')
+    var divLletresGeni = document.getElementById('lletresGeni');
     // div to place the table of the 72 genii names
-    const divLletresGenis72 = document.getElementById('lletresGenis72')
+    var divLletresGenis72 = document.getElementById('lletresGenis72');
     // array with the 72 Genii names of 5 letters each
     // build whith the code in https://gist.github.com/jaumeb/04d74de20d7ea68fc5dc
-    const genis72 = [
-        'והויה', 'יליאל', 'סיטאל', 'עלמיה', 'מהשיה', 'ללהאל', 'אכאיה', 'כהתאל',
-        'הזיאל', 'אלדיה', 'לאויה', 'ההעיה', 'יזלאל', 'מבהאל', 'הריאל', 'הקמיה',
-        'לאויה', 'כליאל', 'לוויה', 'פהליה', 'נלכאל', 'יייאל', 'מלהאל', 'חהויה',
-        'נתהיה', 'האאיה', 'ירתאל', 'שאהיה', 'רייאל', 'אומאל', 'לכבאל', 'ושריה',
-        'יחויה', 'להחיה', 'כוקיה', 'מנדאל', 'אניאל', 'חעמיה', 'רהעאל', 'ייזאל',
-        'הההאל', 'מיכאל', 'ווליה', 'ילהיה', 'סאליה', 'עריאל', 'עשליה', 'מיהאל',
-        'והואל', 'דניאל', 'החשיה', 'עממיה', 'ננאאל', 'ניתאל', 'מבהיה', 'פויאל',
-        'נממיה', 'יילאל', 'הרחאל', 'מצראל', 'ומבאל', 'יההאל', 'ענואל', 'מחיאל',
-        'דמביה', 'מנקאל', 'איעאל', 'חבויה', 'ראהאל', 'יבמיה', 'הייאל', 'מומיה'
-    ]
+    var genis72 = ['והויה', 'יליאל', 'סיטאל', 'עלמיה', 'מהשיה', 'ללהאל', 'אכאיה', 'כהתאל', 'הזיאל', 'אלדיה', 'לאויה', 'ההעיה', 'יזלאל', 'מבהאל', 'הריאל', 'הקמיה', 'לאויה', 'כליאל', 'לוויה', 'פהליה', 'נלכאל', 'יייאל', 'מלהאל', 'חהויה', 'נתהיה', 'האאיה', 'ירתאל', 'שאהיה', 'רייאל', 'אומאל', 'לכבאל', 'ושריה', 'יחויה', 'להחיה', 'כוקיה', 'מנדאל', 'אניאל', 'חעמיה', 'רהעאל', 'ייזאל', 'הההאל', 'מיכאל', 'ווליה', 'ילהיה', 'סאליה', 'עריאל', 'עשליה', 'מיהאל', 'והואל', 'דניאל', 'החשיה', 'עממיה', 'ננאאל', 'ניתאל', 'מבהיה', 'פויאל', 'נממיה', 'יילאל', 'הרחאל', 'מצראל', 'ומבאל', 'יההאל', 'ענואל', 'מחיאל', 'דמביה', 'מנקאל', 'איעאל', 'חבויה', 'ראהאל', 'יבמיה', 'הייאל', 'מומיה'];
 
     // puts cursor in the field for input the number
-    inputNumbers.focus()
+    inputNumbers.focus();
 
-/**
-     * if there is a valid numbers in the input,
-     * execute the function that write the letters.
-     */
+    /**
+         * if there is a valid numbers in the input,
+         * execute the function that write the letters.
+         */
     buttonEscriu.addEventListener('click', function () {
-        let numG = inputNumbers.value
+        var numG = inputNumbers.value;
         if (isValid(numG)) {
-            writeLetters(numG)
+            writeLetters(numG);
         }
-    })
+    });
 
     // capture Return key
     inputNumbers.addEventListener('keydown', function (e) {
-        const key = e.keyCode || e.which // valid in all browsers
+        var key = e.keyCode || e.which; // valid in all browsers
         if (key === 13) {
-            buttonEscriu.click()
-            e.preventDefault() // for Firefox in a form
+            buttonEscriu.click();
+            e.preventDefault(); // for Firefox in a form
         }
-    })
+    });
 
     /**
      * checks out if input is a valid number
@@ -53,14 +44,13 @@ document.addEventListener('DOMContentLoaded', function () {
      */
     function isValid(numG) {
         // only numbers of 1 or 2 digits
-        const numericExpression = /^[0-9][0-9]?$/
+        var numericExpression = /^[0-9][0-9]?$/;
         // from 0 to 72
         if (numG.match(numericExpression) && numG < 73) {
-            return true
+            return true;
         } else {
-            alert("només números de l'1 al 72\n\n" +
-                "escriu 0 per a la taula completa")
-            inputNumbers.focus()
+            alert("només números de l'1 al 72\n\n" + "escriu 0 per a la taula completa");
+            inputNumbers.focus();
         }
     }
 
@@ -71,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
      * @param {number} numG
      */
     function writeLetters(numG) {
-        numG == 0 ? lletres72G(numG) : lletres1G(numG)
+        numG == 0 ? lletres72G(numG) : lletres1G(numG);
     }
     /**
      * write the 5 letters of the Genie
@@ -80,24 +70,18 @@ document.addEventListener('DOMContentLoaded', function () {
     function lletres1G(numG) {
         // the number of one digit always with a leading zero
         // to align the posible list of more than one names
-        let paddedNumber = function (numG) {
-            let nG = parseInt(numG,10)
-            return nG < 10 ? nG =  "&nbsp;&nbsp;" + nG : nG
-        } (numG)
-        divLletresGeni.innerHTML += 
-            `<span style="color: red;">
-            Geni ${paddedNumber}: 
-            <div style="display: inline; float: right; text-align: right; font-size: 2em; color: #727272;">
-            ${genis72[numG - 1]}
-            </div></span><br>`
+        var paddedNumber = function (numG) {
+            var nG = parseInt(numG, 10);
+            return nG < 10 ? nG = "&nbsp;&nbsp;" + nG : nG;
+        }(numG);
+        divLletresGeni.innerHTML += '<span style="color: red;">\n              Geni ' + paddedNumber + ':\n              <div style="display: inline;\n                          float: right;\n                          text-align: right;\n                          font-size: 2em;\n                          color: #727272;" >\n            ' + genis72[numG - 1] + '\n            </div></span>\n            <br>';
     }
 
     /**
      * builds the table of 72 Genii
      */
     function lletres72G() {
-        let table72G =
-            '<table> \
+        var table72G = '<table> \
                 <caption>Els 72 Genis</caption> \
                 <thead> \
                     <tr> \
@@ -112,13 +96,12 @@ document.addEventListener('DOMContentLoaded', function () {
                         <th>#</td> <th>Àngels</td> \
                     </tr> \
                 </thead> \
-                <tbody>' // an order of angels in each column
-        for (let i = 1; i < 9; i++) {
+                <tbody>'; // an order of angels in each column
+        for (var i = 1; i < 9; i++) {
             // builds an eight column table
-            table72G +=
-                    '<tr>\
+            table72G += '<tr>\
                         <td>' + i + '        </td> <td>' + genis72[i - 1] + '</td> \
-                        <td>' + (i + 8)  + ' </td> <td>' + genis72[i + 7] + '</td> \
+                        <td>' + (i + 8) + ' </td> <td>' + genis72[i + 7] + '</td> \
                         <td>' + (i + 16) + ' </td> <td>' + genis72[i + 15] + '</td> \
                         <td>' + (i + 24) + ' </td> <td>' + genis72[i + 23] + '</td> \
                         <td>' + (i + 32) + ' </td> <td>' + genis72[i + 31] + '</td> \
@@ -126,14 +109,11 @@ document.addEventListener('DOMContentLoaded', function () {
                         <td>' + (i + 48) + ' </td> <td>' + genis72[i + 47] + '</td> \
                         <td>' + (i + 56) + ' </td> <td>' + genis72[i + 55] + '</td> \
                         <td>' + (i + 64) + ' </td> <td>' + genis72[i + 63] + '</td> \
-                    </tr>'
+                    </tr>';
         }
 
-        table72G +=
-                '</tbody> \
-            </table>'
-        divLletresGenis72.innerHTML = table72G
+        table72G += '</tbody> \
+            </table>';
+        divLletresGenis72.innerHTML = table72G;
     }
-
-
-}) // DOMContentLoaded
+}); // DOMContentLoaded
